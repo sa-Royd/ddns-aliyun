9d96e2b29df8a0f5296f4fbebf7d2b0ba336a647
sa-Royd <121451576+sa-Royd@users.noreply.github.com>
2022-12-28 01:50:25 +0800

    Update getIpv6Address.js

--- dist/utils/getIpv6Address.js
+++ dist/utils/getIpv6Address.js
@@ -11,15 +11,14 @@ function getIpv6Address() {
     for (let nw in nets) {
         let objArr = nets[nw];
         if (objArr) {
-			objArr.forEach((obj, idx, arr) => {
-				// 没有指定网卡名时，返回所有的有效IPV6地址, 有网卡名时，只返回网卡名对应的IPV6地址
-                if (obj.family === 'IPv6' && obj.address.length > 19 && obj.address.indexOf('fe80') === -1) {
-					// 这里还需要过滤到链路本地的ipv6网址。
-					const newObject = Object.assign(obj, { name: nw });
-					if(!ipv6s.size) ipv6s.set("_default_",newObject);	// 添加一个默认的网卡
-					ipv6s.set(nw, newObject);
-                 }
-			});
+		objArr.forEach((obj, idx, arr) => {
+                	if (obj.family === 'IPv6' && obj.address.length > 19 && obj.address.indexOf('fe80') === -1) {
+				// 这里还需要过滤到链路本地的ipv6网址。
+				const newObject = Object.assign(obj, { name: nw });
+				if(!ipv6s.size) ipv6s.set("_default_",newObject);	// 添加一个默认的网卡
+				ipv6s.set(nw, newObject);
+			}
+		});
         }
         else {
             return null;
